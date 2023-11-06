57b326ba7fd5f51a1d535ef19e0a661cfdc5ac84
sss <89742348+sauc23@users.noreply.github.com>
2023-11-06 14:45:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,24 +16,33 @@ app.get('/stream2', (req, res) => {
   res.redirect('https://stream-relay-geo.ntslive.net/stream2');
 });
 
-// ...
-
 // Add an API route to fetch data from nts.live/api/v2/live
 app.get('/api', (req, res) => {
-  // You can use a library like axios or node-fetch to make the API request.
-  // Here, we'll use node-fetch as an example.
+  // You can use Express's built-in JSON method to make the API request.
 
-  const fetch = require('node-fetch');
+  const https = require('https');
+  const options = {
+    host: 'nts.live',
+    path: '/api/v2/live',
+  };
 
-  fetch('https://nts.live/api/v2/live')
-    .then(response => response.json())
-    .then(data => {
-      res.json(data);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to fetch NTS data' });
+  https.get(options, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
     });
+
+    response.on('end', () => {
+      try {
+        const apiData = JSON.parse(data);
+        res.json(apiData);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch NTS data' });
+      }
+    });
+  });
 });
 
 app.listen(port, () => {

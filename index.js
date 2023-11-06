e4510157359d65b4f15a1d886845914e1b4b7336
sss <89742348+sauc23@users.noreply.github.com>
2023-11-06 14:52:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,45 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Emulate audio streaming for /stream and /stream2 using an HTTP proxy
-app.use('/stream', createProxyMiddleware({
-  target: 'https://stream-relay-geo.ntslive.net',
-  changeOrigin: true,
-  pathRewrite: { '^/stream': '/stream' },
-}));
 
-app.use('/stream2', createProxyMiddleware({
-  target: 'https://stream-relay-geo.ntslive.net',
-  changeOrigin: true,
-  pathRewrite: { '^/stream2': '/stream2' },
-}));
-
-// Add an API route to fetch data from nts.live/api/v2/live
-app.get('/api', (req, res) => {
-  const https = require('https');
-  const options = {
-    host: 'https://nts.live',
-    path: '/api/v2/live',
-  };
-
-  https.get(options, (response) => {
-    let data = '';
-
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      try {
-        const apiData = JSON.parse(data);
-        res.json(apiData.results[0]);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch NTS data' });
-      }
-    });
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
